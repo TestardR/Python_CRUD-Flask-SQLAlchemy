af7527e8bbfd81dd1e03a0159a966bb0e34580a1
Romain Testard <romain.testard@bluenove.com>
2020-09-21 10:23:44 +0200

    I have moved project to sqlAlchemy

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ from flask_jwt import JWT
 from utils.security import authenticate, identity
 from resources.item import Item, ItemList
 from resources.user import UserRegister
+from db.db import db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # To allow flask propagating exception even if debug is set to false on app
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'my_secret'
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.secret_key = "my_secret"
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(UserRegister, '/register')
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    db.init_app(app)
     app.run(port=5000, debug=True)
--- resources/item.py
+++ resources/item.py
@@ -1,18 +1,17 @@
-
 import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
+import sys
 
 from models.item import ItemModel
+from pprint import pprint
 
 
 class Item(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
+    parser.add_argument(
+        "price", type=float, required=True, help="This field cannot be left blank!"
+    )
 
     @jwt_required()
     def get(self, name):
@@ -23,18 +22,18 @@ class Item(Resource):
 
         if item:
             return item.json()
-        return {'message': 'item not found'}, 400
+        return {"message": "item not found"}, 400
 
     def post(self, name):
         if ItemModel.find_by_name(name):
-            return {'message': f'An item with name {name} already exits.'}, 400
+            return {"message": f"An item with name {name} already exits."}, 400
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
-        try:
-            ItemModel.insert()
+        try:    
+            item.save_to_db()
         except:
             return {"message": "An error occured inserting the item."}, 500
 
@@ -43,16 +42,10 @@ class Item(Resource):
     """ @jwt_required() """
 
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
-
-        return {'message': f'Item {name} deleted'}
+        item = Item.find_by_name(name)
+        if item:
+            item.delete_from_db()
+        return {"message": f"Item {name} deleted"}
 
     """ @jwt_required() """
 
@@ -60,25 +53,20 @@ class Item(Resource):
         data = Item.parser.parse_args()
 
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
 
         if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {"message": "An error occured inserting the item."}, 500
+            item = ItemModel(name, data["price"])
         else:
-            try:
-                updated_item.update()
-            except:
-                return {"message": "An error occured updating the item."}, 500
+            item.price = data["price"]
 
-        return updated_item.json()
+        item.save_to_db()
+
+        return item.json()
 
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
+        connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
         query = "SELECT * FROM items"
@@ -87,9 +75,9 @@ class ItemList(Resource):
         items = []
 
         for row in result:
-            items.append({'name': row[0], 'price': row[1]})
+            items.append({"name": row[0], "price": row[1]})
 
         connection.commit()
         connection.close()
 
-        return {'items': items}
+        return {"items": items}
